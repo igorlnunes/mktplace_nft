539326428ee8b37152373cf0ba36620891f0087c
Igor de Lima Nunes <igorlnunes@gmail.com>
2022-02-14 22:37:46 -0300

    Ajustando workflow

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-waffle");
+
+const projectId = "5a2fbabccb774a38bc71d779a22c311f"
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: []
+    },
+    mainnet: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: []
+    },
+  },
+  solidity: "0.8.4",
+};
