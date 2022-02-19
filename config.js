8b318b3f1d21a707dbc9a1244fdf88af56f69c72
Igor de Lima Nunes <igorlnunes@gmail.com>
2022-02-19 14:31:45 -0300

    Done & Mumbai TestNet Configure

---
+++ config.js
@@ -0,0 +1,5 @@
+//update this file everytime that a new hardhat's local node runs 
+
+export const nftaddress = ""//address where nft contract was deployed
+export const nftmarketaddress = ""//address where nft MarketPlace contract was deployed 
+
