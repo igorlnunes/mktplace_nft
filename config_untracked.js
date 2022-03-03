fb89260cf5ea09b83af264da334ee44bf040a1da
Igor de Lima Nunes <igorlnunes@gmail.com>
2022-03-02 22:48:20 -0300

    Versao v0

---
+++ config_untracked.js
@@ -0,0 +1,8 @@
+//update this file everytime that a new hardhat's local node runs 
+
+export const nftaddress = "0xE14F303a73A830fD8A88367Ddce1FDAdC53adFf1"
+export const nftmarketaddress = "0x35F3Be6d591E3b695E0D974aF682491598b22ebF"
+
+//mumbai deploy 19/02/22
+//nftMarket deployed to: 0x35F3Be6d591E3b695E0D974aF682491598b22ebF
+//nft deployed to:  0xE14F303a73A830fD8A88367Ddce1FDAdC53adFf1
